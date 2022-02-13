4aaae1b16a57f1cc2cac1fb9cdfbf14bc9bbf636
Amely Kling <mail@dwi.sk>
2022-02-13 17:30:22 +0100

    Tailwind safelist for lang-grid-cols

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  purge: {
+    options: {
+      safelist: [
+        'md:grid-cols-1', 
+        'md:grid-cols-2', 
+        'md:grid-cols-3', 
+        'md:grid-cols-4', 
+        'md:grid-cols-5', 
+      ],
+    }
+  },
   theme: {
     extend: {},
     screens: {
