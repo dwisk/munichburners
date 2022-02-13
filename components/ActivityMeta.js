99f56a34aba2adb7c9366490a1d4fde6bd10f4ea
Amely Kling <mail@dwi.sk>
2022-02-13 13:53:34 +0100

    Optimized start-enddate if on same day

--- components/ActivityMeta.js
+++ components/ActivityMeta.js
@@ -15,21 +15,31 @@ export default function ActivityMeta({post}) {
           year: "numeric"
         }
       );
-      const enddate = post.properties.Date.date.end && new Date(post.properties.Date.date.end).toLocaleString(
-        "de-DE",
-        post.properties.Date.date.start.length > 10 ? {
-          month: "long",
-          day: "2-digit",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit"
-        } : {
-          month: "long",
-          day: "2-digit",
-          year: "numeric"
-        }
-      );
 
+      let enddate = false;
+      if (post.properties.Date.date.end && post.properties.Date.date.start.length > 10) {
+        enddate = new Date(post.properties.Date.date.end).toLocaleString(
+          "de-DE",
+          post.properties.Date.date.end && post.properties.Date.date.start.substr(0,10) === post.properties.Date.date.end.substr(0,10) ? {
+            hour: "2-digit",
+            minute: "2-digit"
+          } : {
+            month: "long",
+            day: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+          });
+      } else if (post.properties.Date.date.end) {
+        enddate = new Date(post.properties.Date.date.end).toLocaleString(
+          "de-DE",
+          {
+            month: "long",
+            day: "2-digit",
+            year: "numeric",
+          });
+      }
+      
     return (<p className="my-2 text-white">
     <span className="icon-cal">{date}
     {enddate && (
