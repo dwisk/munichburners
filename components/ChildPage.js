331d3959d8b84859f0f23b40dac2fee3a81d2ff5
Amely Kling <mail@dwi.sk>
2022-05-15 21:42:56 +0200

    refresh childpage cache

--- components/ChildPage.js
+++ components/ChildPage.js
@@ -1,4 +1,5 @@
-import { usePageBlocks } from "../lib/_clienthelpers";
+import { useRouter } from "next/router";
+import { revalidatePage, usePageBlocks } from "../lib/_clienthelpers";
 import Blocks from "./Blocks";
 
 export default function ChildPage (props) {
@@ -12,13 +13,28 @@ export default function ChildPage (props) {
     const className = hasCover ? `childpage ${page.id} dsk:py-20 dsk:mt-20 relative pt-20 bx-container mb:bg-none bg-fixed bg-cover bg-center` : "py-20";
     const style = hasCover ? {backgroundImage: `url('${hasCover}')`} : {};
 
+    const reval = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        refresh();
+    }
+
+    const router = useRouter();
+
+    const refresh = async () => {
+        console.info("refresh childpage cache");
+        await revalidatePage(props.page.id);
+        router.replace(router.asPath, router.asPath, { scroll: false }); // soft refresh
+      };
+    
+
     return <div className={className} style={style}  >
         <h1 className="page-h1 py-4 mt-20 dsk:mt-0">
             <a href={`/page/${page.id}`}>
                 {loading ? childPage.child_page?.title : page?.properties?.title?.title[0].plain_text}
             </a>
         </h1>
-        <section className="panel dsk:mb-0">
+        <section className="panel dsk:mb-0"  onDoubleClick={reval}>
             {loading ? (
                 <div>...</div>
             ) : (
