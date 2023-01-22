20d9c1645560a7e9744fe9c29d1693a44dd3b57f
Amely Kling <mail@dwi.sk>
2023-01-22 14:51:40 +0100

    font subset latin

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { Montserrat_Alternates } from '@next/font/google'
 
 const burnerFont = Montserrat_Alternates({
   weight: ["400", "900"],
+  subsets: ['latin'],
   display: "auto",
   variable: '--burner-font',
 });
