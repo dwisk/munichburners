08c9cc1ba4b968171b6791019c5c4c3e3eb38f7c
Amely Kling <mail@dwi.sk>
2023-03-12 23:40:38 +0100

    Remove Old Survival guide

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     async redirects() {
       return [
-        { source: '/survivalguide', destination: '/page/38902cca-d0f2-41e3-801d-84dc3d51381f', permanent: true,},
       ]
     },
     }
+    
