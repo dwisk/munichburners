2e1b4d0ec59cd0b6ecb3ed7e9b69436256bcc68d
Amely Kling <mail@dwi.sk>
2023-03-12 23:35:22 +0100

    New Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,39 @@
 import { LanguageProvider } from "../lib/LanguageContext";
 import "../styles/globals.css";
-import { Montserrat_Alternates } from '@next/font/google'
+import { Tourney, Noto_Sans } from '@next/font/google'
 
-const burnerFont = Montserrat_Alternates({
-  weight: ["400", "900"],
+const titleFont = Tourney({
+  weight: ["700"],
   subsets: ['latin'],
   display: "auto",
-  variable: '--burner-font',
+  variable: '--title-font',
+});
+
+const textFont = Noto_Sans({
+  weight: ["200","400","800"],
+  subsets: ['latin'],
+  display: "auto",
+  variable: '--text-font',
 });
 
 
 function MyApp({ Component, pageProps }) {
-  return   <main  className={burnerFont.className}>
+  return<>
+  <style jsx global>{`
+        html {
+          font-family: ${textFont.style.fontFamily};
+        }
+        h1,h2,h3,h4, .h2{
+          font-family: ${titleFont.style.fontFamily};
+        }
+      `}</style>
+  
+     <main>
     <LanguageProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps}/>
     </LanguageProvider>
-  </main>;
+  </main></>
+  ;
 }
 
 export default MyApp;
