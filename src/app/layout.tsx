1d5a6c74a84ef7d83ec6f70db97d64f7e59ebed1
Amely Kling <mail@dwi.sk>
2025-02-27 19:00:27 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { LanguageProvider } from 'munichburners/lib/LanguageContext';
 import '../styles/globals.css'
 import { textFont, titleFont } from './fonts';
+import Head from 'next/head';
 interface props {
     children: React.ReactNode;
 }
@@ -8,6 +9,25 @@ interface props {
 export default async function Layout({ children }:props) {
     return (
         <html lang="en" className={`${textFont.variable} ${titleFont.variable}`}>
+            <head>
+                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+                <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <meta name="msapplication-TileColor" content="#ffffff"/>
+                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+                <meta name="theme-color" content="#221111"/>
+            </head>
             <body>
                 <LanguageProvider>
                     {children}
